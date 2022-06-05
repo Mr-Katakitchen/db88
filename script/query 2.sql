5d5ea0de2386bc51d5df190e2bcd518b6082e5b8
Mr-Katakitchen <100071393+Mr-Katakitchen@users.noreply.github.com>
2022-06-05 14:26:48 +0300

    Add files via upload

--- script/query 2.sql
+++ script/query 2.sql
@@ -1,9 +1,26 @@
-SELECT *
-FROM proj_per_res 
-WHERE res_id < 100
+#Opsi Deyteri
+
+SELECT title, department #SELECT program 
+FROM program
+GROUP BY prog_id;
+
+SELECT program_title, department, project_title, budget, started_on, ends_on
+FROM proj_per_prog
+WHERE program_title = 'Adams Inc'
 GROUP BY proj_id;
 
-SELECT *
-FROM proj_per_org 
-WHERE org_id < 50
-GROUP BY proj_id;
+#Opsi Prwti
+
+SELECT concat(first_name, ' ', last_name) AS researcher_name, gender, birthdate, name AS organization_name #SELECT researcher 
+FROM researcher 
+INNER JOIN organization ON researcher.org_id = organization.org_id 
+GROUP BY res_id;
+
+SELECT concat(first_name, ' ', last_name) AS researcher_name, title AS project_title, budget, started_on, ends_on
+FROM project 
+INNER JOIN works_on ON works_on.proj_id = project.proj_id 
+INNER JOIN researcher ON researcher.res_id = works_on.res_id 
+WHERE concat(first_name, ' ', last_name) = 'Verile D''Adda'
+GROUP BY project.proj_id;
+
+
